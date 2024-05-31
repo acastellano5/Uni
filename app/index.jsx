350d86d85191ef1a0001afc0b6517b1038d2ac6a
NReidD <nathan.reid@salesianum.org>
2024-05-31 14:23:14 -0400

    Revert "Merge branch 'main' into andrew"
    This reverts commit bb7d4d119ccc53cb86e2411e0df57383d48854a7, reversing
    changes made to e6d0a72ce5b7d6ba9fcd7a5f55255eb47ab0d10a.

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,36 @@
-import { Redirect } from "expo-router";
+import { View, Text, Image, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { FontAwesome6 } from '@expo/vector-icons';
+// import logo  from "../assets/images/logo.png"
+import logo from "../assets/images/logo.png"
 
-const StartPage = () => {
-  return <Redirect href="/home" />;
+const Onboarding = () => {
+  return (
+    <SafeAreaView className="bg-black h-full">
 
+      <View className="pl-9">
+        <Text className="text-primary text-4xl font-bold">Uni</Text>
+      </View>
+
+
+      <View className="bg-white mt-5 h-full">
+        <View>
+            <Image
+              source={logo}
+              resizeMode="contain"
+              className="w-[130px] h-[84px]"
+            />
+        </View>
+
+
+        <FontAwesome6 name="school" size={100} color="black" />
+      </View>
+
+    <StatusBar backgroundColor="#000" style="light"/>
+    </SafeAreaView>
+  )
 }
-export default StartPage;
+
+export default Onboarding
--- assets/constants/icons.js
+++
@@ -1,6 +0,0 @@
-import { Entypo } from '@expo/vector-icons';
-const home = <Entypo name="home" size={24} color="black" />
-export{
-
-    home
-}
