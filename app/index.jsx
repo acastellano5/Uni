e6d0a72ce5b7d6ba9fcd7a5f55255eb47ab0d10a
Andrew Castellano <castellano.andrew05@gmail.com>
2024-05-31 14:09:45 -0400

    onboarding screen update

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { FontAwesome6 } from '@expo/vector-icons';
+// import logo  from "../assets/images/logo.png"
+import logo from "../assets/images/logo.png"
 
-export default function App() {
+const Onboarding = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView className="bg-black h-full">
+
+      <View className="pl-9">
+        <Text className="text-primary text-4xl font-bold">Uni</Text>
+      </View>
+
+
+      <View className="bg-white mt-5 h-full">
+        <View>
+            <Image
+              source={logo}
+              resizeMode="contain"
+              className="w-[130px] h-[84px]"
+            />
+        </View>
+
+
+        <FontAwesome6 name="school" size={100} color="black" />
+      </View>
+
+    <StatusBar backgroundColor="#000" style="light"/>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Onboarding
