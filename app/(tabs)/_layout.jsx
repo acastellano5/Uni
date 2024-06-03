962a4424bebb5f5c8aac347fad8338c7e537b9f0
Andrew Castellano <castellano.andrew05@gmail.com>
2024-06-03 11:05:40 -0400

    modified tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,25 +1,120 @@
-import React from "react";
-import { StyleSheet, Text, View } from 'react-native'
-import Icons from "../../assets/icons/Icons";
-import {Home, Social, Clubs, Events} from './index';
+import { StatusBar } from "expo-status-bar";
+import { Redirect, Tabs } from "expo-router";
+import { Image, Text, View } from "react-native";
 
+// import { icons } from "../../constants";
+// import { Loader } from "../../components";
+// import { useGlobalContext } from "../../context/GlobalProvider";
 
-import Colors from "../../assets/Colors";
-import TabBar from "../../components/TabBar";
-const TabArr = [
-    { route: 'Home', label: 'Home', type: Icons.Feather, icon: 'home', component: Home, color: Colors.primary, alphaClr: Colors.primaryAlpha },
-    { route: 'Clubs', label: 'Search', type: Icons.Feather, icon: 'search', component: Clubs, color: Colors.green, alphaClr: Colors.greenAlpha },
-    { route: 'Social', label: 'Add New', type: Icons.Feather, icon: 'plus-square', component: Events, color: Colors.red, alphaClr: Colors.redAlpha },
-    { route: 'Events', label: 'Account', type: Icons.FontAwesome, icon: 'user-circle-o', component: Social, color: Colors.purple, alphaClr: Colors.purpleAlpha },
-  ];
-  
-  const MainLayout= () => {
-    return (
-     <TabBar
-     Tabs={TabArr} />
-    )
-  }
-  
-  const styles = StyleSheet.create({})
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
-export default MainLayout
+const TabLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#FFF",
+            borderTopWidth: 1,
+            height: 84,
+
+            // shadow styling
+            shadowOffset: {
+              width: 0,
+              height: 12,
+            },
+            shadowOpacity: 0.58,
+            shadowRadius: 16.0,
+            elevation: 24
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                // icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="messages"
+          options={{
+            title: "Messages",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                // icon={icons.bookmark}
+                color={color}
+                name="Bookmark"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                // icon={icons.plus}
+                color={color}
+                name="Create"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                // icon={icons.profile}
+                color={color}
+                name="Profile"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+
+      <StatusBar backgroundColor="#000" style="light" />
+    </>
+  );
+};
+
+export default TabLayout;
