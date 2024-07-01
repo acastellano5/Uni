e329d76209e62cde51b102275adfc914b5e2ad4e
NReidD <nathan.reid@salesianum.org>
2024-07-01 08:42:05 -0400

    Group Routes

--- context/globalProvider.js
+++ context/globalProvider.js
@@ -1,5 +1,5 @@
 import React, {createContext, useContext, useEffect, useState} from "react";
-
+import auth from '@react-native-firebase/auth'
 import {getCurrentUser} from "../lib/firebase";
 const GlobalContext = createContext()
 export const useGlobalContext = () => useContext(GlobalContext);
@@ -7,7 +7,7 @@ export const useGlobalContext = () => useContext(GlobalContext);
 const GlobalProvider = ({children}) => {
     const [isLogged, setIsLogged] = useState(false);
     const [initializing, setInitializing] = useState(true);
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState();
     const [loading, setLoading] = useState(true);
     const [isVerified, setIsVerified] = useState(false);
     const [orgId, setOrgId] = useState(20030049);
@@ -15,43 +15,35 @@ const GlobalProvider = ({children}) => {
 
     
 
-    useEffect(() => {
-        getCurrentUser().then((res) => {
-            //console.log(res);
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+        if (user) {
+            setIsVerified(true)
+            setIsLogged(true)
+            setLoading(false)
+            if (user.emailVerified) {
+                setIsVerified(true)
+            } else {
+                setIsLogged(true)
+                setIsVerified(false)
+            }        
+        }
+        else{
+            setIsVerified(false)
+            setIsLogged(false)
+            setLoading(false)
+            setUser(user)
+        }
+      }
+    
+      useEffect(() => {
+        console.log("WHYY");
+        const subscriber = auth().onUserChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+      }, []);
 
-            if (res) {
-                
 
-                //console.log(res);
-                if (!res.emailVerified) {
-                    setIsLogged(false)
-                    setIsVerified(false)
-                    setUser(res)
-
-                }
-                else
-                {
-                    setIsLogged(true)
-                    setIsVerified(true)
-                    setUser(res)
-                    /*console.log('====================================');
-                    console.log("ATT");
-                    console.log('====================================');
-                    */
-                }
-                
-            } 
-            
-            else {
-                setIsLogged(false)
-                setUser(res)
-            }
-        }).catch((error)=> {
-            console.log(error);
-        }).finally(()=> {
-            setLoading(false);
-        })
-    })
     return (
         <GlobalContext.Provider
         value={{
