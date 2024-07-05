60564e79d7300d06311f63ef1cef0a0475ff6b4c
Andrew Castellano <castellano.andrew05@gmail.com>
2024-07-05 16:22:38 -0400

    added join/leave functionality to groups page

--- app/(tabs)/clubs/clubHome.jsx
+++ app/(tabs)/clubs/clubHome.jsx
@@ -5,6 +5,7 @@ import {
   ImageBackground,
   TouchableOpacity,
   StyleSheet,
+  Alert,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useLocalSearchParams } from "expo-router";
@@ -14,14 +15,19 @@ import TabsDisplay from "../../../components/TabsDisplay";
 import ClubInfo from "../../../components/clubs/ClubInfo";
 import ClubMembers from "../../../components/clubs/ClubMembers";
 import BackHeader from "../../../components/BackHeader";
-import { getGroupById, joinGroup } from "../../../lib/useFirebase";
+import {
+  getGroupById,
+  joinGroup,
+  isUserInGroup,
+  leaveGroup,
+} from "../../../lib/useFirebase";
 import { useGlobalContext } from "../../../context/globalProvider";
 
 const tabs = ["Info", "Members"];
 
 const ClubHome = () => {
-   // getting orgId from global context
-   const { orgId } = useGlobalContext();
+  // getting orgId from global context
+  const { orgId } = useGlobalContext();
 
   // retrieve params from request
   const params = useLocalSearchParams();
@@ -33,34 +39,77 @@ const ClubHome = () => {
   // setting club state
   const [club, setClub] = useState(null);
 
+  // function gets club data
+  const fetchClub = async () => {
+    if (!id) {
+      console.log("ID is not available");
+      return;
+    }
+
+    // console.log("Fetching club data for ID:", id);
+    try {
+      const clubData = await getGroupById(id, orgId);
+      if (clubData) {
+        // console.log("Club data fetched:", clubData);
+        setClub(clubData);
+      } else {
+        console.log("No club data found");
+      }
+    } catch (error) {
+      console.error("Error fetching club data:", error);
+    }
+  };
+
   // fetching club data
   useEffect(() => {
-    const fetchClub = async () => {
-      if (!id) {
-        console.log("ID is not available");
-        return;
-      }
-
-      // console.log("Fetching club data for ID:", id);
-      try {
-        const clubData = await getGroupById(id, orgId);
-        if (clubData) {
-          // console.log("Club data fetched:", clubData);
-          setClub(clubData);
-        } else {
-          console.log("No club data found");
-        }
-      } catch (error) {
-        console.error("Error fetching club data:", error);
-      }
-    };
-
     fetchClub();
   }, [id]);
 
+  // isInGroup state
+  const [isInGroup, setIsInGroup] = useState(false);
+
   useEffect(() => {
-    console.log(club)
-  }, [club])
+    // fetch to see if the user is in the group or not
+    const fetchStatus = async () => {
+      if (club) {
+        const status = await isUserInGroup(club.id, orgId);
+        setIsInGroup(status);
+      }
+    };
+    fetchStatus();
+  }, [club]);
+
+  useEffect(() => {
+    console.log("Is the user in this group? " + isInGroup);
+  }, [isInGroup]);
+
+  const handleJoinOrLeaveGroup = async () => {
+    if (isInGroup) {
+      Alert.alert(
+        "Leave Group",
+        "Are you sure you want to leave this group?",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          {
+            text: "Yes",
+            onPress: async () => {
+              await leaveGroup(orgId, club.id);
+              setIsInGroup(false);
+              await fetchClub(); // Fetch the updated club members list
+            },
+          },
+        ]
+      );
+    } else {
+      await joinGroup(orgId, club.id);
+      setIsInGroup(true);
+      await fetchClub(); // Fetch the updated club members list
+    }
+  };
 
   const displayTabContent = () => {
     switch (activeTab) {
@@ -68,7 +117,9 @@ const ClubHome = () => {
         return <ClubInfo club={club} />;
 
       case "Members":
-        return <ClubMembers members={club.members} moderators={club.moderators} />;
+        return (
+          <ClubMembers members={club.members} moderators={club.moderators} />
+        );
 
       default:
         return null;
@@ -86,7 +137,10 @@ const ClubHome = () => {
   return (
     <ScrollView showsVerticalScrollIndicator={false} className="bg-white">
       {/* club background image */}
-      <ImageBackground className="w-full h-[40vh] pb-5" source={{uri: `${club.image}`}}>
+      <ImageBackground
+        className="w-full h-[40vh] pb-5"
+        source={{ uri: `${club.image}` }}
+      >
         <SafeAreaView className="w-11/12 mx-auto h-full justify-between z-10">
           {/* back button and ai btn */}
           <BackHeader />
@@ -94,17 +148,19 @@ const ClubHome = () => {
           {/* club name and join button */}
           <View className="flex-row justify-between items-center">
             <View className="bg-tertiary py-2 px-4 rounded">
-              <Text className="text-white text-lg font-semibold">{club.name}</Text>
+              <Text className="text-white text-lg font-semibold">
+                {club.name}
+              </Text>
             </View>
 
             <TouchableOpacity
-            onPress={()=>{
-              joinGroup(orgId,id)
-            }}
+              onPress={handleJoinOrLeaveGroup}
               className="bg-white py-2 px-4 rounded"
               activeOpacity={0.8}
             >
-              <Text className="text-primary text-lg font-semibold">Join</Text>
+              <Text className="text-primary text-lg font-semibold">
+                {isInGroup ? "Joined" : "Join"}
+              </Text>
             </TouchableOpacity>
           </View>
         </SafeAreaView>
