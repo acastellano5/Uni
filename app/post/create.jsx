9cf79d3b678ce2cae17641985fce58a55f467761
NReidD <nathan.reid@salesianum.org>
2024-07-10 12:45:03 -0400

    fix create

--- app/post/create.jsx
+++ app/post/create.jsx
@@ -38,7 +38,7 @@ const create = () => {
   const [image, setImage] = useState(null);
   const [form, setForm] = useState({
     caption: "",
-    postUrl: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
+    postUrl: "",
   });
 
   useEffect(() => {
