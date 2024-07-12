bf542049691ca4e75bc820781eded1d7a0abb24a
Andrew Castellano <castellano.andrew05@gmail.com>
2024-07-12 14:05:53 -0400

    working on stuff

--- app/group/index.jsx
+++ app/group/index.jsx
@@ -21,7 +21,8 @@ import {
   leaveGroup,
   ifGroupFollowed,
   unfollowGroup, 
-  followGroup
+  followGroup,
+  isUserModerator
 } from "../../lib/useFirebase";
 import { useGlobalContext } from "../../context/globalProvider";
 import { AntDesign } from '@expo/vector-icons';
@@ -70,11 +71,16 @@ const GroupHome = () => {
 
   // isInGroup state
   const [isInGroup, setIsInGroup] = useState(false);
-  //
+  // see if they are following group
   const [isFollowingGroup, setIsFollowingGroup] = useState(false);
 
+
+  // see if the currentUser is moderator
+  const [ isModerator, setIsModerator ] = useState(false)
+
+
   useEffect(() => {
-    // fetch to see if the user is in the group or not
+    // set the statuses of user
     const fetchStatus = async () => {
       if (group) {
         const status = await isUserInGroup(group.id, orgId);
@@ -82,13 +88,16 @@ const GroupHome = () => {
 
         const isFollowing = await ifGroupFollowed(group.id, orgId);
         setIsFollowingGroup(isFollowing);
+
+        const isGroupModerator = await isUserModerator(group.id, orgId)
+        setIsModerator(isGroupModerator)
       }
     };
     fetchStatus();
   }, [group]);
 
   const handleJoinOrLeaveGroup = async () => {
-    if (isInGroup) {
+    if (isInGroup || isModerator) {
       Alert.alert("Leave Group", "Are you sure you want to leave this group?", [
         {
           text: "Cancel",
@@ -145,71 +154,82 @@ const GroupHome = () => {
   }
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false} className="bg-white">
-      {/* group background image */}
-      <ImageBackground
-        className="w-full h-[40vh] pb-5"
-        source={{ uri: `${group.image}` }}
-      >
-        <SafeAreaView className="w-11/12 mx-auto h-full justify-between z-10">
-          {/* back button and ai btn */}
-          <BackHeader />
+    <View style={{ flex: 1 }}>
+      <ScrollView showsVerticalScrollIndicator={false} className="bg-white">
+        {/* group background image */}
+        <ImageBackground
+          className="w-full h-[40vh] pb-5"
+          source={{ uri: `${group.image}` }}
+        >
+          <SafeAreaView className="w-11/12 mx-auto h-full justify-between z-10">
+            {/* back button and ai btn */}
+            <BackHeader />
 
-          {/* group name and join button */}
-          <View className="flex-row justify-between items-center">
-            {/* group name */}
-            <View className="bg-tertiary py-2 px-4 rounded" style={{ maxWidth: "60%" }}>
-              <Text className="text-white text-base font-semibold">
-                {group.name}
-              </Text>
-            </View>
-
-            {/* buttons */}
-            <View className="flex-row items-center">
-              {!isInGroup ? (
-                <TouchableOpacity
-                  className="bg-white p-2 rounded mr-2"
-                  activeOpacity={0.8}
-                  onPress={handleFollowOrUnfollowGroup}
-                >
-                  {isFollowingGroup ? (
-                    <AntDesign name="star" size={24} color="#22c55e" />
-                  ) : (
-                    <AntDesign name="staro" size={24} color="#22c55e" />
-                  )}
-                </TouchableOpacity>
-              ) : null}
-
-              <TouchableOpacity
-                onPress={handleJoinOrLeaveGroup}
-                className="bg-white py-2 px-3 rounded"
-                activeOpacity={0.8}
-              >
-                <Text className="text-primary text-base font-semibold">
-                  {isInGroup ? "Leave" : "Join"}
+            {/* group name and join button */}
+            <View className="flex-row justify-between items-center">
+              {/* group name */}
+              <View className="bg-tertiary py-2 px-4 rounded" style={{ maxWidth: "60%" }}>
+                <Text className="text-white text-base font-semibold">
+                  {group.name}
                 </Text>
-              </TouchableOpacity>
+              </View>
+
+              {/* buttons */}
+              <View className="flex-row items-center">
+                {!isInGroup && !isModerator ? (
+                  <TouchableOpacity
+                    className="bg-white p-2 rounded mr-2"
+                    activeOpacity={0.8}
+                    onPress={handleFollowOrUnfollowGroup}
+                  >
+                    {isFollowingGroup ? (
+                      <AntDesign name="star" size={24} color="#22c55e" />
+                    ) : (
+                      <AntDesign name="staro" size={24} color="#22c55e" />
+                    )}
+                  </TouchableOpacity>
+                ) : null}
+
+                <TouchableOpacity
+                  onPress={handleJoinOrLeaveGroup}
+                  className="bg-white py-2 px-3 rounded"
+                  activeOpacity={0.8}
+                >
+                  <Text className="text-primary text-base font-semibold">
+                    {isInGroup || isModerator? "Leave" : "Join"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
-          </View>
-        </SafeAreaView>
+          </SafeAreaView>
 
-        <View style={styles.overlay} />
-      </ImageBackground>
+          <View style={styles.overlay} />
+        </ImageBackground>
 
-      {/* group info */}
-      <View className="bg-darkWhite mt-5 h-full rounded-t-3xl pt-5 pb-11 bottom-10">
-        <TabsDisplay
-          tabs={tabs}
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-          containerStyles="py-3"
-          textStyles="text-lg"
-          tabBarStyles="mb-6 w-11/12"
-        />
+        {/* group info */}
+        <View className="bg-darkWhite mt-5 h-full rounded-t-3xl pt-5 pb-11 bottom-10">
+          <TabsDisplay
+            tabs={tabs}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+            containerStyles="py-3"
+            textStyles="text-lg"
+            tabBarStyles="mb-6 w-11/12"
+          />
 
-        {displayTabContent()}
-      </View>
-    </ScrollView>
+          {displayTabContent()}
+        </View>
+      </ScrollView>
+      
+      {isModerator ? (
+        <TouchableOpacity
+        style={styles.floatingButton}
+        onPress={() => Alert.alert('Button Pressed!')}
+      >
+        <AntDesign name="plus" size={24} color="white" />
+      </TouchableOpacity>
+      ) : null}
+    </View>
   );
 };
 
@@ -217,6 +237,17 @@ const styles = StyleSheet.create({
   overlay: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: "rgba(0,0,0,0.2)",
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 70,
+    right: 20,
+    backgroundColor: '#22c55e',
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
