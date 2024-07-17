b2ac2e390336143b9c11faafb25351ad5e97d793
NReidD <nathan.reid@salesianum.org>
2024-07-17 09:01:28 -0400

    Filters near ready

--- components/social/AlumniFilter.jsx
+++ components/social/AlumniFilter.jsx
@@ -5,9 +5,18 @@ import SingleSelectDropdown from "../dropdown/SingleSelect";
 import MultiSelectDropDown from "../dropdown/MultiSelect";
 import { interestsData, classData, collegesData, statesData, jobFieldsData } from "../../assets/data"
 import CustomButton from "../CustomButton";
-
+import { filterUsers } from "../../lib/useFirebase";
+import { useGlobalContext } from "../../context/globalProvider";
 const AlumniFilter = () => {
+  const { orgId} = useGlobalContext();
 
+  const [form, setForm] = useState({
+    class: "",
+    interests: [],
+    state: "",
+    field: "",
+    college: "",
+  });
   return (
     <>
       <View className="mt-3 mb-3 flex-row justify-center items-center">
@@ -16,15 +25,15 @@ const AlumniFilter = () => {
       </View>
     
     <View className="w-11/12 mx-auto mb-5">
-        <SingleSelectDropdown data={classData} focusedColor="#22c55e" title="Class" placeholder="Filter by Class"/>
-        <MultiSelectDropDown title="Interests" placeholder="Filter by Interests" data={interestsData}/>
-        <SingleSelectDropdown data={statesData} focusedColor="#22c55e" title="State" placeholder="Filter by State"/>
-        <SingleSelectDropdown data={jobFieldsData} focusedColor="#22c55e" title="Field" placeholder="Filter by Field"/>
-        <SingleSelectDropdown data={collegesData} focusedColor="#22c55e" title="College" placeholder="Filter by College"/>
+        <SingleSelectDropdown data={classData} onItemSelect={(e) => {setForm({ ...form, class: e.value })}} focusedColor="#22c55e" title="Class" placeholder="Filter by Class"/>
+        <MultiSelectDropDown title="Interests" setList={(e) => {setForm({ ...form, interests: e })}} placeholder="Filter by Interests" data={interestsData}/>
+        <SingleSelectDropdown data={statesData} onItemSelect={(e) => setForm({ ...form, state: e.value })} focusedColor="#22c55e" title="State" placeholder="Filter by State"/>
+        <SingleSelectDropdown data={jobFieldsData} onItemSelect={(e) => setForm({ ...form, field: e.value })} focusedColor="#22c55e" title="Field" placeholder="Filter by Field"/>
+        <SingleSelectDropdown data={collegesData} onItemSelect={(e) => setForm({ ...form, college: e.value })} focusedColor="#22c55e" title="College" placeholder="Filter by College"/>
     </View>
 
 
-    <CustomButton title="Apply" containerStyles="bg-primary py-3 w-4/12 mx-auto" textStyles="text-white font-semibold"/>    
+    <CustomButton title="Apply" containerStyles="bg-primary py-3 w-4/12 mx-auto" textStyles="text-white font-semibold" handlePress={()=> {console.log(form),filterUsers("Alumni",form, orgId)}}/>    
     </>
   );
 };
