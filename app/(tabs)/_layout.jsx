62fd07984947cc1df451f0c432dca2622e57bf08
NReidD <nathan.reid@salesianum.org>
2024-07-21 13:26:58 -0400

    format edit

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,20 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { Redirect, Tabs } from "expo-router";
 import { router } from "expo-router";
+import Home from "./home";
+import Messages from "./messages";
+import Groups from "./groups";
+import EventsPage from "./events";
+import Social from "./social";
 import { Image, Text, View } from "react-native";
 import { useGlobalContext } from "../../context/globalProvider";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // import { icons } from "../../constants";
 // import { Loader } from "../../components";
 // import { useGlobalContext } from "../../context/GlobalProvider";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { SimpleLineIcons } from '@expo/vector-icons';
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 const TabIcon = ({ icon, color, name, focused, iconType }) => {
-
   return (
     <View className="flex items-center justify-center gap-2">
-      { iconType === "Ionicons" ? (
+      {iconType === "Ionicons" ? (
         <Ionicons name={`${icon}`} size={24} color={`${color}`} />
       ) : (
         <SimpleLineIcons name={`${icon}`} size={24} color={`${color}`} />
@@ -31,127 +36,119 @@ const TabIcon = ({ icon, color, name, focused, iconType }) => {
 };
 
 const TabLayout = () => {
-  const {loading, isLogged, isVerified} = useGlobalContext();
-  if (!loading && isLogged && isVerified){
-  
+  const { loading, isLogged, isVerified } = useGlobalContext();
+  if (!loading && isLogged && isVerified) {
   } else {
     router.replace("//index");
-
-  
   }
-
+  const Tab = createBottomTabNavigator();
   // if (!loading && !isLogged) router.replace('//index');
 
   return (
     <>
-        <Tabs
-          screenOptions={{
-            tabBarActiveTintColor: "#22c55e",
-            tabBarInactiveTintColor: "#CDCDE0",
-            tabBarShowLabel: false,
-            tabBarStyle: {
-              backgroundColor: "#FFF",
-              borderTopWidth: 1,
-              height: 96,
-              paddingTop: 10,
-
-              // shadow styling
-              shadowOffset: {
-                width: 0,
-                height: 12,
-              },
-              shadowOpacity: 0.58,
-              shadowRadius: 16.0,
-              elevation: 24,
-            },
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#22c55e",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#FFF",
+            borderTopWidth: 1,
+            height: 96,
+            paddingTop: 10,
+          },
+          shadowOffset: {
+            width: 0,
+            height: 12,
+          },
+          shadowOpacity: 0.58,
+          shadowRadius: 16.0,
+          elevation: 24,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon="home"
+                iconType="Ionicons"
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
           }}
-        >
-          <Tabs.Screen
-            name="home"
-            options={{
-              title: "Home",
-              headerShown: false,
-              tabBarIcon: ({ color, focused }) => (
-                <TabIcon
-                  icon="home"
-                  iconType="Ionicons"
-                  color={color}
-                  name="Home"
-                  focused={focused}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="messages"
-            options={{
-              title: "Messages",
-              headerShown: false,
-
-              tabBarIcon: ({ color, focused }) => (
-                <TabIcon
-                  icon="chatbubble"
-                  iconType="Ionicons"
-                  color={color}
-                  name="Messages"
-                  focused={focused}
-                />
-              ),
-            }}
-          />
-
-          <Tabs.Screen
-            name="groups"
-            options={{
-              title: "Groups",
-              headerShown: false,
-              tabBarIcon: ({ color, focused }) => (
-                <TabIcon
-                  icon="organization"
-                  iconType="SimpleLineIcons"
-                  color={color}
-                  name="Groups"
-                  focused={focused}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="events"
-            options={{
-              title: "Events",
-              headerShown: false,
-              tabBarIcon: ({ color, focused }) => (
-                <TabIcon
-                  icon="calendar-sharp"
-                  iconType="Ionicons"
-                  color={color}
-                  name="Events"
-                  focused={focused}
-                />
-              ),
-            }}
-          />
-
-          <Tabs.Screen
-            name="social"
-            options={{
-              title: "Social",
-              headerShown: false,
-              tabBarIcon: ({ color, focused }) => (
-                <TabIcon
-                  icon="people-sharp"
-                  iconType="Ionicons"
-                  color={color}
-                  name="Social"
-                  focused={focused}
-                />
-              ),
-            }}
-          />
-        </Tabs>
-
-        <StatusBar backgroundColor="#000" style="light" />
+        />
+        <Tab.Screen
+          name="Messaging"
+          component={Messages}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon="chatbubble"
+                iconType="Ionicons"
+                color={color}
+                name="Messages"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Groups"
+          component={Groups}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon="organization"
+                iconType="SimpleLineIcons"
+                color={color}
+                name="Groups"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Events"
+          component={EventsPage}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon="calendar-sharp"
+                iconType="Ionicons"
+                color={color}
+                name="Events"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Social"
+          component={Social}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon="people-sharp"
+                iconType="Ionicons"
+                color={color}
+                name="Social"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+      <StatusBar backgroundColor="#000" style="light" />
     </>
   );
 };
--- app/post/create.jsx
+++ app/post/create.jsx
@@ -1,149 +1,149 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import React, { useState, useEffect } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
-import BackHeader from "../../components/BackHeader";
-import FormField from "../../components/FormField";
-import CustomButton from "../../components/CustomButton";
-import * as ImagePicker from "expo-image-picker";
-import {
-  createUserPost,
-  createGroupPost,
-  uploadToFirebase,
-} from "../../lib/useFirebase";
-import { useGlobalContext } from "../../context/globalProvider";
-import { router, useLocalSearchParams } from "expo-router";
-
-const CreatePost = () => {
-  const { orgId } = useGlobalContext();
-  const { authorType, groupId } = useLocalSearchParams();
-  const [image, setImage] = useState(null);
-  const [form, setForm] = useState({
-    caption: "pool day",
-    postUrl:
-      "https://leisurepoolsusa.com/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",
-  });
-
-  useEffect(() => {
-    console.log(image);
-  }, [image]);
-
-  const pickImage = async () => {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== "granted") {
-      alert("Sorry, we need camera roll permissions to make this work!");
-      return;
-    }
-
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
-  const takePhoto = async () => {
-    const { status } = await ImagePicker.requestCameraPermissionsAsync();
-    if (status !== "granted") {
-      alert("Sorry, we need camera permissions to make this work!");
-      return;
-    }
-
-    let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
-  return (
-    <SafeAreaView className="h-full bg-secondary">
-      <BackHeader containerStyles="w-11/12 mx-auto" />
-      <View className="bg-darkWhite mt-5 h-full rounded-t-3xl pt-5 pb-10">
-        <View className="w-10/12 mx-auto top-[50]">
-          <Text className="text-3xl text-center font-semibold">Create Post</Text>
-
-          <FormField
-            title="Caption"
-            value={form.caption}
-            handleChangeText={(e) => setForm({ ...form, caption: e })}
-            otherStyles="mb-5"
-            placeholder="Caption..."
-            labelStyles="text-base font-medium"
-            isEditable={true}
-          />
-
-
-          <Text className="text-base font-medium mb-2">Image (optional)</Text>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={pickImage} className="border border-primary" activeOpacity={0.8}>
-              <Ionicons name="image-outline" size={24} color="#22c55e" />
-              <Text style={styles.buttonText} className="text-primary">Pick an Image</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={takePhoto} className="border border-primary" activeOpacity={0.8}>
-              <Ionicons name="camera-outline" size={24} color="#22c55e" />
-              <Text style={styles.buttonText} className="text-primary">Take a Photo</Text>
-            </TouchableOpacity>
-          </View>
-
-          {image && <Image source={{ uri: image }} style={styles.image} />}
-
-          <CustomButton
-            title="Create"
-            containerStyles="bg-primary py-3"
-            textStyles="text-white text-base font-semibold"
-            handlePress={async () => {
-              if (authorType === "group") {
-                createGroupPost(groupId, form.postUrl, form.caption, orgId);
-              } else if (authorType === "user") {
-                if (image) {
-                  const task = await uploadToFirebase(image);
-                  createUserPost(task, form.caption, orgId);
-                } else {
-                  createUserPost(form.postUrl, form.caption, orgId);
-                }
-              }
-              router.push("/home");
-            }}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
-};
-
-export default CreatePost;
-
-const styles = StyleSheet.create({
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginBottom: 15,
-  },
-  button: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 8,
-  },
-  buttonText: {
-    marginLeft: 10,
-  },
-  image: {
-    width: 200,
-    height: 200,
-    alignSelf: "center",
-    marginBottom: 15,
-  }
-});
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";  
+import React, { useState, useEffect } from "react";  
+import { SafeAreaView } from "react-native-safe-area-context";  
+import { Ionicons } from "@expo/vector-icons";  
+import BackHeader from "../../components/BackHeader";  
+import FormField from "../../components/FormField";  
+import CustomButton from "../../components/CustomButton";  
+import * as ImagePicker from "expo-image-picker";  
+import {  
+  createUserPost,  
+  createGroupPost,  
+  uploadToFirebase,  
+} from "../../lib/useFirebase";  
+import { useGlobalContext } from "../../context/globalProvider";  
+import { router, useLocalSearchParams } from "expo-router";  
+  
+const CreatePost = () => {  
+  const { orgId } = useGlobalContext();  
+  const { authorType, groupId } = useLocalSearchParams();  
+  const [image, setImage] = useState(null);  
+  const [form, setForm] = useState({  
+    caption: "pool day",  
+    postUrl:  
+      "https://leisurepoolsusa.com/wp-content/uploads/2020/06/best-type-of-swimming-pool-for-my-home_2.jpg",  
+  });  
+  
+  useEffect(() => {  
+    console.log(image);  
+  }, [image]);  
+  
+  const pickImage = async () => {  
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();  
+    if (status !== "granted") {  
+      alert("Sorry, we need camera roll permissions to make this work!");  
+      return;  
+    }  
+  
+    let result = await ImagePicker.launchImageLibraryAsync({  
+      mediaTypes: ImagePicker.MediaTypeOptions.All,  
+      allowsEditing: true,  
+      aspect: [4, 3],  
+      quality: 1,  
+    });  
+  
+    if (!result.canceled) {  
+      setImage(result.assets[0].uri);  
+    }  
+  };  
+  
+  const takePhoto = async () => {  
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();  
+    if (status !== "granted") {  
+      alert("Sorry, we need camera permissions to make this work!");  
+      return;  
+    }  
+  
+    let result = await ImagePicker.launchCameraAsync({  
+      allowsEditing: true,  
+      aspect: [4, 3],  
+      quality: 1,  
+    });  
+  
+    if (!result.canceled) {  
+      setImage(result.assets[0].uri);  
+    }  
+  };  
+  
+  return (  
+    <SafeAreaView className="h-full bg-secondary">  
+      <BackHeader containerStyles="w-11/12 mx-auto" />  
+      <View className="bg-darkWhite mt-5 h-full rounded-t-3xl pt-5 pb-10">  
+        <View className="w-10/12 mx-auto top-[50]">  
+          <Text className="text-3xl text-center font-semibold">Create Post</Text>  
+  
+          <FormField  
+            title="Caption"  
+            value={form.caption}  
+            handleChangeText={(e) => setForm({ ...form, caption: e })}  
+            otherStyles="mb-5"  
+            placeholder="Caption..."  
+            labelStyles="text-base font-medium"  
+            isEditable={true}  
+          />  
+  
+  
+          <Text className="text-base font-medium mb-2">Image (optional)</Text>  
+          <View style={styles.buttonContainer}>  
+            <TouchableOpacity style={styles.button} onPress={pickImage} className="border border-primary" activeOpacity={0.8}>  
+              <Ionicons name="image-outline" size={24} color="#22c55e" />  
+              <Text style={styles.buttonText} className="text-primary">Pick an Image</Text>  
+            </TouchableOpacity>  
+  
+            <TouchableOpacity style={styles.button} onPress={takePhoto} className="border border-primary" activeOpacity={0.8}>  
+              <Ionicons name="camera-outline" size={24} color="#22c55e" />  
+              <Text style={styles.buttonText} className="text-primary">Take a Photo</Text>  
+            </TouchableOpacity>  
+          </View>  
+  
+          {image && <Image source={{ uri: image }} style={styles.image} />}  
+  
+          <CustomButton  
+            title="Create"  
+            containerStyles="bg-primary py-3"  
+            textStyles="text-white text-base font-semibold"  
+            handlePress={async () => {  
+              if (authorType === "group") {  
+                createGroupPost(groupId, form.postUrl, form.caption, orgId);  
+              } else if (authorType === "user") {  
+                if (image) {  
+                  const task = await uploadToFirebase(image);  
+                  createUserPost(task, form.caption, orgId);  
+                } else {  
+                  createUserPost(form.postUrl, form.caption, orgId);  
+                }  
+              }  
+              router.push("/home");  
+            }}  
+          />  
+        </View>  
+      </View>  
+    </SafeAreaView>  
+  );  
+};  
+  
+export default CreatePost;  
+  
+const styles = StyleSheet.create({  
+  buttonContainer: {  
+    flexDirection: "row",  
+    justifyContent: "space-around",  
+    marginBottom: 15,  
+  },  
+  button: {  
+    flexDirection: "row",  
+    alignItems: "center",  
+    paddingVertical: 10,  
+    paddingHorizontal: 20,  
+    borderRadius: 8,  
+  },  
+  buttonText: {  
+    marginLeft: 10,  
+  },  
+  image: {  
+    width: 200,  
+    height: 200,  
+    alignSelf: "center",  
+    marginBottom: 15,  
+  }  
+});  
