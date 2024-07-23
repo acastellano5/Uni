08f7d3f7cf0fe74f340b9d8927ae25ab2853ec0b
Andrew Castellano <castellano.andrew05@gmail.com>
2024-07-23 16:58:28 -0400

    fixed info box

--- components/profile/InfoBox.jsx
+++ components/profile/InfoBox.jsx
@@ -5,7 +5,7 @@ import { router } from "expo-router";
 const Item = ({ name, title }) => {
   return (
     <View style={title !== "Groups" ? styles.itemStyles : null}>
-      <View className="bg-lightGreen rounded-lg py-3">
+      <View className="bg-lightGreen rounded-lg py-3 flex-1 justify-center">
         <Text className="text-center">{name}</Text>
       </View>
     </View>
@@ -16,21 +16,30 @@ const InfoBox = ({ title, info }) => {
   return (
     <View className="mb-3">
       <Text className="text-lg font-medium mb-1">{title}</Text>
-      <View className="flex-row flex-wrap">
+      <View className="flex-row flex-wrap" style={{alignItems: "stretch"}}>
         {info && info.length > 0 ? (
           title === "Groups" ? (
             info.map((item, index) => (
-            <TouchableOpacity activeOpacity={0.8} key={index} style={styles.itemStyles} onPress={() => {
-              router.push({
-                pathname: "/group",
-                params: { id: item.id }
-              })
-            }}>
-              <Item name={item.name} title={title}/>
-            </TouchableOpacity>
-          ))
+              <View style={styles.itemStyles}>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  key={index}
+                  className="flex-1 bg-lightGreen rounded-lg py-3 justify-center"
+                  onPress={() => {
+                    router.push({
+                      pathname: "/group",
+                      params: { id: item.id },
+                    });
+                  }}
+                >
+                  <Text className="text-center">{item.name}</Text>
+                </TouchableOpacity>
+              </View>
+            ))
           ) : (
-            info.map((item, index) => <Item key={index} name={item} title={title}/>)
+            info.map((item, index) => (
+              <Item key={index} name={item} title={title} />
+            ))
           )
         ) : (
           <View style={styles.noInfoContainer}>
