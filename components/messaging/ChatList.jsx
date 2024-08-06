5bbefd41e3e14c4666502fc40049e70abff4d801
TylerSelden <tyler.selden@salesianum.org>
2024-08-05 20:44:26 -0400

    Quick commit

--- components/messaging/ChatList.jsx
+++ components/messaging/ChatList.jsx
@@ -47,13 +47,17 @@ const ChatList = ({ filter }) => {
           ) : (
             <View>
             {filter == "DMs" ? <AIChatMessageCard lastMsgData={"Hello! How can I help you today?"}></AIChatMessageCard> : null}
-            {chats.map((chat) => (
+            {(chats.length > 0) ? chats.map((chat) => (
               <View>
                 <MessageCard lastMsgData={chat.lastMsg.msg} lastMsgTimestamp={chat.lastMsg.timestamp} author={chat.lastMsg.author} users={chat.users} chatID={chat.id} />
                 {/* Placeholder at bottom, because of menu */}
                 <View style={{ height: 120 }} />
               </View>
-            ))}
+            )) : (
+              <View className="flex items-center justify-center mt-10">
+                <Text className="text-[#333] text-lg font-semibold">Your {filter} will appear here.</Text>
+              </View>
+            )}
           </View>
         )}
       </ScrollView>
@@ -63,11 +67,11 @@ const ChatList = ({ filter }) => {
 }
 
 const MessageCard = ({ lastMsgData, lastMsgTimestamp, author, users, chatID }) => {
-  const shortStr = (str, len) => {
-    return (str.length > len) ? str.slice(0, len).trim() + "..." : str;
-  }
   return(
-     <TouchableOpacity className="w-full flex-row items-center justify-start py-2 mt-2" activeOpacity={.6} onPress={() => router.push("/aiChat")}>
+     <TouchableOpacity className="w-full flex-row items-center justify-start py-2 mt-2" activeOpacity={.6} onPress={() => router.push({
+        pathname: "/chat",
+        params: { id: chatID }
+     })}>
       <View className="rounded-full flex items-center border-2 border-primary ml-5 mr-1 justify-center">
         <FontAwesome name="user-circle" size={50} color="black" />
       </View>
