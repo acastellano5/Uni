59d880885bf8f21c6785f82e6da8ba548f5b1936
Andrew Castellano <castellano.andrew05@gmail.com>
2024-08-14 12:31:33 -0400

    Update schoolShow.jsx

--- app/(auth)/accountSetUp/schoolShow.jsx
+++ app/(auth)/accountSetUp/schoolShow.jsx
@@ -4,10 +4,11 @@ import {
   View,
   ScrollView,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import SingleSelect from "../../../components/dropdown/SingleSelect";
 import { router } from "expo-router";
 import { roles } from "../../../assets/data";
@@ -22,11 +23,65 @@ const SchoolShow = () => {
     college: "",
     state: "",
     fieldOfEmployment: "",
+    schoolEmail: "",
+    studentEmail: "",
   });
   const [role, setRole] = useState("");
 
-  
-  // displays different fields depending on what role the user selects when trying to join an organization
+  const validateFields = () => {
+    if (role === "Student" || role === "Faculty/Staff") {
+      return form.schoolEmail.trim() !== "";
+    } else if (role === "Parent") {
+      return form.studentEmail.trim() !== "";
+    } else if (role === "Alumni") {
+      return (
+        form.class &&
+        form.college &&
+        form.state &&
+        form.fieldOfEmployment
+      );
+    }
+    return true;
+  };
+
+  const handleFormSubmit = () => {
+    if (!validateFields()) {
+      Alert.alert("Error", "Please fill out all required fields.");
+      return;
+    }
+
+    switch (role) {
+      case "Student":
+      case "Faculty/Staff":
+        router.push({
+          pathname: './verifyEmail',
+          params: { role: "school member" }
+        });
+        break;
+      case "Parent":
+        router.push('./verifyEmail');
+        break;
+      case "Guest":
+        // Perform any action needed for Guest
+        break;
+      case "Alumni":
+        sendAlumniRequest(null, {
+          class: form.class.label,
+          classValue: form.class.label,
+          state: form.state.label,
+          stateValue: form.state.value,
+          employment: form.fieldOfEmployment.label,
+          employmentValue: form.fieldOfEmployment.value,
+          college: form.college.label,
+          collegeValue: form.college.value
+        }, 20030049)
+          .then(() => router.push("./processReq"));
+        break;
+      default:
+        break;
+    }
+  };
+
   const displayFields = () => {
     switch (role) {
       case "Student":
@@ -37,15 +92,14 @@ const SchoolShow = () => {
               title="School email address"
               placeholder="name@schoolemail.com"
               isEditable={true}
+              value={form.schoolEmail}
+              handleChangeText={(e) => setForm({ ...form, schoolEmail: e })}
             />
             <CustomButton
               title="Verify"
               containerStyles="bg-greenTheme py-2 mt-8"
               textStyles="text-white text-base"
-              handlePress={() => router.push({
-                pathname: './verifyEmail',
-                params: {role: "school member"}
-              })}
+              handlePress={handleFormSubmit}
             />
           </>
         );
@@ -57,12 +111,14 @@ const SchoolShow = () => {
               title="Student's email address"
               placeholder="name@schoolemail.com"
               isEditable={true}
+              value={form.studentEmail}
+              handleChangeText={(e) => setForm({ ...form, studentEmail: e })}
             />
             <CustomButton
               title="Verify"
               containerStyles="bg-greenTheme py-2 mt-8"
               textStyles="text-white text-base"
-              handlePress={() => router.push('./verifyEmail')}
+              handlePress={handleFormSubmit}
             />
           </>
         );
@@ -73,47 +129,69 @@ const SchoolShow = () => {
             title="Join"
             containerStyles="bg-greenTheme py-2"
             textStyles="text-white text-base"
+            handlePress={handleFormSubmit}
           />
         );
 
       case "Alumni":
-
         return (
           <>
-            <SingleSelect title="Class" placeholder="Select class" data={classData} containerStyles="mb-2"selectedValue={form.class} // Add this line
-            onItemSelect={(item) => {
-              setForm({
-                ...form,
-                class: item,
-              });
-            }}/>
-            <SingleSelect title="College" placeholder="Select college" data={collegesData} containerStyles="mb-2"selectedValue={form.college} // Add this line
-            onItemSelect={(item) => {
-              setForm({
-                ...form,
-                college: item,
-              });
-            }}/>
-            <SingleSelect title="State" placeholder="Select State" data={statesData} containerStyles="mb-2"selectedValue={form.state} // Add this line
-            onItemSelect={(item) => {
-              setForm({
-                ...form,
-                state: item,
-              });
-            }}/>
-            <SingleSelect title="Field of employment" placeholder="Select field of employment" data={jobFieldsData}selectedValue={form.fieldOfEmployment} // Add this line
-            onItemSelect={(item) => {
-              
-              setForm({
-                ...form,
-                fieldOfEmployment: item,
-              });
-            }}/>
+            <SingleSelect
+              title="Class"
+              placeholder="Select class"
+              data={classData}
+              containerStyles="mb-2"
+              selectedValue={form.class}
+              onItemSelect={(item) => {
+                setForm({
+                  ...form,
+                  class: item,
+                });
+              }}
+            />
+            <SingleSelect
+              title="College"
+              placeholder="Select college"
+              data={collegesData}
+              containerStyles="mb-2"
+              selectedValue={form.college}
+              onItemSelect={(item) => {
+                setForm({
+                  ...form,
+                  college: item,
+                });
+              }}
+            />
+            <SingleSelect
+              title="State"
+              placeholder="Select State"
+              data={statesData}
+              containerStyles="mb-2"
+              selectedValue={form.state}
+              onItemSelect={(item) => {
+                setForm({
+                  ...form,
+                  state: item,
+                });
+              }}
+            />
+            <SingleSelect
+              title="Field of employment"
+              placeholder="Select field of employment"
+              data={jobFieldsData}
+              selectedValue={form.fieldOfEmployment}
+              onItemSelect={(item) => {
+                setForm({
+                  ...form,
+                  fieldOfEmployment: item,
+                });
+              }}
+            />
             <CustomButton
               title="Join"
               containerStyles="bg-greenTheme py-2 mt-8"
               textStyles="text-white text-base"
-              handlePress={() => {sendAlumniRequest(null,{class: form.class.label, classValue: form.class.label, state: form.state.label, stateValue: form.state.value, employment: form.fieldOfEmployment.label, employmentValue: form.fieldOfEmployment.value,college: form.college.label, collegeValue: form.college.value},20030049).then(router.push("./processReq"))}}
+              handlePress={handleFormSubmit}
             />
           </>
         );
