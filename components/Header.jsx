e73cd801dc8801e90a9c5bf54759e860890c29bb
Andrew Castellano <castellano.andrew05@gmail.com>
2024-11-26 22:23:31 -0500

    fixed profile picture in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,40 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/images/logo.png";
 import { FontAwesome } from "@expo/vector-icons";
 import { router } from "expo-router";
 import Entypo from '@expo/vector-icons/Entypo';
+import { useGlobalContext } from "../context/globalProvider";
+import { getUserAttributes } from "../lib/useFirebase";
+import defaultPic from "../assets/images/profilepic.jpeg"
 
 const Header = ({ textStyles }) => {
+  const { user } = useGlobalContext()
+  const [ profilePic, setProfilePic ] = useState("")
+
+  useEffect(() => {
+    const fetchUserPic = async () => {
+      const userAttr = await getUserAttributes(user.uid)
+      setProfilePic(userAttr.profilePicture)
+    }
+
+    fetchUserPic()
+  }, [ ])
+
   return (
     <View className="flex-row justify-between w-11/12 mx-auto items-center">
       <TouchableOpacity
         activeOpacity={0.8}
         onPress={() => router.push("/profile/profileShow")}
       >
-        <FontAwesome name="user-circle" size={27} color="white" />
+         <Image
+              source={profilePic ? { uri: profilePic } : defaultPic}
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+              }}
+            />
       </TouchableOpacity>
 
         <Text
